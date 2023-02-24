41e0b23deb0d21087aaac5fdb67505abca536611
Vishnu.M.M <vishnumm2003@gmail.com>
2023-02-24 21:09:23 +0530

    Precaution for COmplaint View

--- Complaint.dart
+++ Complaint.dart
@@ -39,8 +39,8 @@ class ComplaintPage extends StatefulWidget {
 
 class _ComplaintPageState extends State<ComplaintPage> {
   TextEditingController Complaint = TextEditingController();
-complaint(String C)
-async {
+
+complaint(String C) async {
   final pref=await SharedPreferences.getInstance();
   String ip= pref.getString("ip").toString();
   String uid=pref.getString("lid").toString();
@@ -57,37 +57,36 @@ async {
       String lid_s=jsondata["lid"].toString();
       final prefs = await SharedPreferences.getInstance();
       prefs.setString("lid", lid_s);
-      // Fluttertoast.showToast(
-      //   msg: "Success",
-      //   toastLength: Toast.LENGTH_SHORT,
-      //   gravity: ToastGravity.CENTER,
-      //   timeInSecForIosWeb: 1,
-      //   backgroundColor: Colors.blue,
-      //   textColor: Colors.white,
-      //   fontSize: 16.0,
-      // );
-
-      print("tost");
       setState(() {
         Complaint.text='';
       });
-
     }
-  else
-    {
-      print("tost");
-      // Fluttertoast.showToast(
-      //     msg: "Status is not ok",
-      //     toastLength: Toast.LENGTH_SHORT,
-      //     gravity: ToastGravity.CENTER,
-      //     timeInSecForIosWeb: 1,
-      //     backgroundColor: Colors.red,
-      //     textColor: Colors.white,
-      //     fontSize: 16.0,
-      // );
-    }
-
 }
+
+
+// View Complaint
+  String ips='';
+  Future<List<ViewComplaint>> _getNames() async {
+    final pref = await SharedPreferences.getInstance();
+    String ip = pref.getString("ip").toString();
+    String pid=pref.getString("pid_s").toString();
+    var data = await http.post(Uri.parse("http://" + ip + ":5000/and_view_complaint"),body: {"pid":pid});
+    // print("------------------------------hoiiiiiii---------------");
+    // print(data);
+    var jsonData = json.decode(data.body);
+    setState(() {
+      ips=ip.toString();
+    });
+
+    // print(jsonData);
+    List<ViewComplaint> clist = [];
+    for (var nn in jsonData["data"]) {
+      ViewComplaint newname =
+      ViewComplaint(nn["user_lid"].toString(),nn["complaint"].toString(),nn["date"].toString());
+      clist.add(newname);
+    }
+    return clist;
+  }
 
 
   @override
@@ -105,63 +104,40 @@ async {
         title: Text(widget.title),
       ),
       body:
-        Column(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      child: TextField( controller: Complaint, decoration: InputDecoration( hintText: 'Complaints'),),
-                    ),
+      Column(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    child: TextField( controller: Complaint, decoration: InputDecoration( hintText: 'Complaints'),),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(onPressed: (){
-                      String Complaint_v=Complaint.text;
-                      complaint(Complaint_v);
-                    }, child: Text("Sent")),
-                  )
-                          ],
-                        ),
-                      ),
-            SizedBox(
-              child: Column(
-                children: [
-                  Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("User Name",
-                            style: TextStyle(
-                              fontSize: 20
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(250, 8, 8, 10),
-                          child: Text("Date",
-                          style: TextStyle(
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(onPressed: (){
+                    String Complaint_v=Complaint.text;
+                    complaint(Complaint_v);
+                  }, child: Text("Sent")),
+                )
+              ],
+            ),
+          ),
 
-                          )),
-                        ),
-                      ],
-                    ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-                  child: Text("Complaint",
-                    style: TextStyle(
-                      fontSize: 30
-                    ),
-                  )
-            ),
-           ], ),
-            ),
-          ],
-        ),
-              );
+
+        ],
+      ),
+    );
     // This trailing comma makes auto-formatting nicer for build method
   }
+}
+
+class ViewComplaint {
+  late final user_lid ;
+  late final complaint ;
+  late final date ;
+  ViewComplaint(this.user_lid,this.complaint,this.date);
 }
